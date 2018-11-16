c9118042bc31c4f631a07f726634187c30229be0
Frank Chang <shchang@gmail.com>
2018-11-16 23:51:34 +0800

    hello world

--- converter_summary/Program.cs
+++ converter_summary/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace converter_summary
 {
@@ -9,6 +10,25 @@ namespace converter_summary
     {
         static void Main(string[] args)
         {
+            string iniFile = "d:"+ Path.DirectorySeparatorChar + "CONVERTER_SUMMARY.INI";
+            string seconds = "20";
+            string counts = "60";
+            if (args.Count() > 0)
+            {
+                test(args[0]);
+            }
+            
+            Console.WriteLine("test");
+            Console.ReadKey();
+        }
+
+        static void test(string path)
+        {
+            Console.WriteLine("trying path: " + path);
+            if (File.Exists(path))
+                Console.WriteLine("path  found");
+            else
+                Console.WriteLine("path not found");
         }
     }
 }
